acef00a718aea54d23a0e702495988452bc8e113
Pedro Bortolli <pedro.vitor.santos@usp.br>
2019-04-27 20:47:54 +0000

    Add custom hook to retrieve screen size at every resize

---
+++ src/hooks/useScreenSize.js
@@ -0,0 +1,20 @@
+import React, {useState, useEffect} from 'react'
+
+function useScreenSize() {
+	const [width, changeWidth] = useState(window.innerWidth)
+	const [height, changeHeight] = useState(window.innerHeight)
+	const handleResize = () => {
+		changeWidth(window.innerWidth)
+		changeHeight(window.innerHeight)
+	}
+
+	useEffect(() => {
+		window.addEventListener('resize', handleResize)
+		return () => {
+			window.removeEventListener('resize', handleResize)
+		}
+	})
+	return [width, height]
+}
+
+export default useScreenSize
