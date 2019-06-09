72cfb10f969bb23918700d6f15fbbaca097ea71d
Pedro Bortolli <pedro.vitor.santos@usp.br>
2019-06-09 03:25:28 +0000

    Add points to header

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,12 @@
-import React, {useEffect} from 'react'
+import React, {useState, useEffect} from 'react'
 import {Link} from 'react-router-dom'
 import {getCredentials, assertAuth} from '../auth/services'
 import logo from '../assets/funtball-logo.png'
 import styled from 'styled-components'
 import {primaryColor} from '../utils/constants'
+import fetchApi from '../api/fetch'
+
+const url = 'http://localhost:5000/'
 
 const Container = styled.div`
 	display: flex;
@@ -17,23 +20,34 @@ const MenuOptions = styled.div`
 	flex-direction: row;
 	align-items: center;
 	justify-content: flex-end;
-	* {
+	> * {
 		margin-left: 32px;
+		font-size: 22px;
 	}
 `
 
 function Header() {
+	const [userPoints, setUserPoints] = useState(null)
+	useEffect(() => {
+		const fetchPoints = async () => {
+			const pts = await fetchApi('GET', url + 'get-points/pedro')
+			setUserPoints(pts.points)
+		}
+		fetchPoints()
+	}, [])
+	
 	return (
 		<div>
 			<Container>
 				<img style={{float: 'left'}} src={logo} width="230" height="60" />
 				<MenuOptions>
-					<Link to="/ranking" style={{fontSize: 22, color: primaryColor}}>Ranking</Link>
-					<Link to="/dashboard" style={{fontSize: 22, color: primaryColor}}>Dashboard</Link>
+					<a style={{color: primaryColor}}>{userPoints || 0} Points</a>
+					<Link to="/ranking" style={{color: primaryColor}}>Ranking</Link>
+					<Link to="/dashboard" style={{color: primaryColor}}>Dashboard</Link>
 					{getCredentials() ?
-						<Link to="/logoff" style={{fontSize: 22, color: primaryColor}}>Logoff</Link>
+						<Link to="/logoff" style={{color: primaryColor}}>Logoff</Link>
 						:
-						<Link to="/login" style={{fontSize: 22, color: primaryColor}}>Login</Link>
+						<Link to="/login" style={{color: primaryColor}}>Login</Link>
 					}
 				</MenuOptions>
 			</Container>
