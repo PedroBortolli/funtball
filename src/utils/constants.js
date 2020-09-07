59542f31537173e3692d670670c5da7d1ad3e320
Pedro Bortolli <pedrovbortolli@gmail.com>
2020-09-07 19:41:56 -0300

    Update week finder for 2020 season

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,21 +4,21 @@ export const url = 'https://funtball-backend.herokuapp.com'
 
 export const getWeek = () => {
     const current = + new Date()
-    if (current < + new Date(2019, 8, 10, 5)) return 1
-    if (current < + new Date(2019, 8, 17, 5)) return 2
-    if (current < + new Date(2019, 8, 24, 5)) return 3
-    if (current < + new Date(2019, 9, 1, 5)) return 4
-    if (current < + new Date(2019, 9, 8, 5)) return 5
-    if (current < + new Date(2019, 9, 15, 5)) return 6
-    if (current < + new Date(2019, 9, 22, 5)) return 7
-    if (current < + new Date(2019, 9, 29, 5)) return 8
-    if (current < + new Date(2019, 10, 5, 5)) return 9
-    if (current < + new Date(2019, 10, 12, 5)) return 10
-    if (current < + new Date(2019, 10, 19, 5)) return 11
-    if (current < + new Date(2019, 10, 26, 5)) return 12
-    if (current < + new Date(2019, 11, 3, 5)) return 13
-    if (current < + new Date(2019, 11, 10, 5)) return 14
-    if (current < + new Date(2019, 11, 17, 5)) return 15
-    if (current < + new Date(2019, 11, 24, 5)) return 16
+    if (current < + new Date(2020, 8, 15, 5)) return 1
+    if (current < + new Date(2020, 8, 22, 5)) return 2
+    if (current < + new Date(2020, 8, 29, 5)) return 3
+    if (current < + new Date(2020, 9, 6, 5)) return 4
+    if (current < + new Date(2020, 9, 13, 5)) return 5
+    if (current < + new Date(2020, 9, 20, 5)) return 6
+    if (current < + new Date(2020, 9, 29, 5)) return 7
+    if (current < + new Date(2020, 10, 3, 5)) return 8
+    if (current < + new Date(2020, 10, 10, 5)) return 9
+    if (current < + new Date(2020, 10, 17, 5)) return 10
+    if (current < + new Date(2020, 10, 24, 5)) return 11
+    if (current < + new Date(2020, 11, 1, 5)) return 12
+    if (current < + new Date(2020, 11, 8, 5)) return 13
+    if (current < + new Date(2020, 11, 15, 5)) return 14
+    if (current < + new Date(2020, 11, 22, 5)) return 15
+    if (current < + new Date(2020, 11, 31, 5)) return 16
     return 17
 }
